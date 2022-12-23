54378bfe90f18e98de2f890874c88e64d44c7070
Clever <arnez.4292@gmail.com>
2022-12-23 18:58:41 -0400

    promesas

--- src/bases/06-importexport.js
+++ src/bases/06-importexport.js
@@ -1,16 +1,18 @@
-import heroes, { owners } from "./data/heroes";
+import heroes, { owners } from "../data/heroes";
 
-console.log(heroes);
+// console.log(heroes);
 
 // FIND solo devuelve el primer elemento que cumple la condición
 const getHeroeById = (id) => {
   return heroes.find((heroe) => heroe.id === id);
 };
-console.log(getHeroeById(2));
+// console.log(getHeroeById(2));
 
 // FILTER devuelve un nuevo arreglo con todos los elementos que cumplan la condición
 const getHeroesByOwner = (owner) => {
   return heroes.filter((heroe) => heroe.owner === owner);
 };
-console.log(getHeroesByOwner("DC"));
-console.log(owners);
+// console.log(getHeroesByOwner("DC"));
+// console.log(owners);
+
+export { getHeroeById, getHeroesByOwner };
