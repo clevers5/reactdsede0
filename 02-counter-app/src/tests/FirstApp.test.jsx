80b2760d4ded54436fc68dc00fb146a2815aa711
Clever <arnez.4292@gmail.com>
2023-01-03 23:09:53 -0400

    FirstApp Test

--- 02-counter-app/src/tests/FirstApp.test.jsx
+++ 02-counter-app/src/tests/FirstApp.test.jsx
@@ -4,7 +4,23 @@ import {FirstApp} from '../FirstApp';
 describe('Pruebas en FirstApp', () => { 
     test('Debe de hacer match con el snapshot', () => {
         const title = "Hola mundo";
-        render(<FirstApp title={title}/>)
-         
+        const { container } = render(<FirstApp title={title}/>);
+         console.log(container);
+
+
+         //Para hacer match con el snapshot se usa el siguiente comando:
+        expect(container).toMatchSnapshot();
     })
- })
+
+    test('Debe de mostrar el titulo en un h1', () => {
+        const title = "Hola mundo";
+        const { container, getByText } = render(<FirstApp title={title}/>);
+        
+        expect(getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
+        //toContain sirve para buscar un texto dentro de un string sin importar los espacios
+    })
+ 
+})
